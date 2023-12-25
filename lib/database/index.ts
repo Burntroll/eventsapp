0e42fd77f64c383659f9b589528f7bb7ba1e72d9
burntroll <fco.silva.dev@gmail.com>
2023-12-25 14:56:44 +0300

    Create DB connection and models

--- lib/database/index.ts
+++ lib/database/index.ts
@@ -6,4 +6,17 @@ let cached = (global as any).mongoose || { conn: null, promise: null };
 
 export const connectedToDatabase = async () => {
   if (cached.conn) return cached.conn;
+
+  if (!MONGODB_URI) throw new Error("MONGODB_URI is missing");
+
+  cached.promise =
+    cached.promise ||
+    mongoose.connect(MONGODB_URI, {
+      dbName: "keys2ville",
+      bufferCommands: false,
+    });
+
+  cached.conn = await cached.promise;
+
+  return cached.conn;
 };

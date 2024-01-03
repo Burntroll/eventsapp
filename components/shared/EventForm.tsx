3d3e6443fefd5fb71f73379340bd0f310ddedfde
burntroll <fco.silva.dev@gmail.com>
2024-01-03 09:51:40 +0300

    Add Event Location

--- components/shared/EventForm.tsx
+++ components/shared/EventForm.tsx
@@ -20,6 +20,7 @@ import { eventDefaultValues } from "@/constants";
 import Dropdown from "./Dropdown";
 import { Textarea } from "@/components/ui/textarea";
 import { FileUploader } from "./FileUploader";
+import Image from "next/image";
 
 const formSchema = z.object({
   username: z.string().min(2, {
@@ -117,6 +118,33 @@ const EventForm = ({ userId, type }: EventFormProps) => {
                     imageUrl={field.value}
                     setFiles={setFiles}
                   />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+        </div>
+
+        <div className="flex flex-col gap-5 md:flex-row">
+          <FormField
+            control={form.control}
+            name="location"
+            render={({ field }) => (
+              <FormItem className="w-full">
+                <FormControl>
+                  <div className="flex-center h-[54px] w-full overflow-hidden rounded-full bg-gray-50 px-4 py-2">
+                    <Image
+                      src="/assets/icons/location-grey.svg"
+                      alt="calendar"
+                      width={24}
+                      height={24}
+                    />
+                    <Input
+                      placeholder="Event location or Online"
+                      {...field}
+                      className="input-field"
+                    />
+                  </div>
                 </FormControl>
                 <FormMessage />
               </FormItem>

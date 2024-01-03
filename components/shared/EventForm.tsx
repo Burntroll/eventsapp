712ff4541a8c10ed3b77d96a72ea4b5e1563eb00
burntroll <fco.silva.dev@gmail.com>
2024-01-03 15:05:49 +0300

    Add onSubmit function logic

--- components/shared/EventForm.tsx
+++ components/shared/EventForm.tsx
@@ -22,9 +22,11 @@ import { FileUploader } from "./FileUploader";
 import { useState } from "react";
 import Image from "next/image";
 import DatePicker from "react-datepicker";
+import { useUploadThing } from "@/lib/uploadthing";
 
 import "react-datepicker/dist/react-datepicker.css";
 import { Checkbox } from "@/components/ui/checkbox";
+import { useRouter } from "next/navigation";
 
 const formSchema = z.object({
   username: z.string().min(2, {
@@ -40,6 +42,9 @@ type EventFormProps = {
 const EventForm = ({ userId, type }: EventFormProps) => {
   const [files, setFiles] = useState<File[]>([]);
   const initialValues = eventDefaultValues;
+  const router = useRouter();
+
+  const { startUpload } = useUploadThing("imageUploader");
 
   // 1. Define your form.
   const form = useForm<z.infer<typeof eventFormSchema>>({
@@ -48,10 +53,37 @@ const EventForm = ({ userId, type }: EventFormProps) => {
   });
 
   // 2. Define a submit handler.
-  function onSubmit(values: z.infer<typeof eventFormSchema>) {
-    // Do something with the form values.
-    // ✅ This will be type-safe and validated.
-    console.log(values);
+  async function onSubmit(values: z.infer<typeof eventFormSchema>) {
+    const eventData = values;
+
+    let uploadedImageUrl = values.imageUrl;
+
+    if (files.length > 0) {
+      const uploadedImages = await startUpload(files);
+
+      if (!uploadedImages) {
+        return;
+      }
+
+      uploadedImageUrl = uploadedImages[0].url;
+
+      if (type === "Create") {
+        try {
+          const newEvent = await createEvent({
+            event: { ...values, imageUrl: uploadedImageUrl },
+            userId,
+            path: "/profile",
+          });
+
+          if (newEvent) {
+            form.reset();
+            router.push(`/events/${newEvent._id}`);
+          }
+        } catch (error) {
+          console.log(error);
+        }
+      }
+    }
   }
 
   return (
